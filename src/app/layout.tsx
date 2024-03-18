e77b77f2f7d5da0027aada1d909c981eee88c759
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-03-18 16:12:41 +0100

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
@@ -37,6 +38,7 @@ export default function RootLayout({
           </main>
         </body>
         <SpeedInsights />
+        <Analytics />
       </html>
     </ClerkProvider>
   );
