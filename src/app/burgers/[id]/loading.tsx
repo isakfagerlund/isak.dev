dc72dfffbe4ce24d702e15348c72d506662754f6
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-03-21 16:22:45 +0100

    fix

--- src/app/burgers/[id]/loading.tsx
+++ src/app/burgers/[id]/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "~/app/_components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="m-auto flex h-full max-w-[600px] flex-col justify-between py-6">
+    <div className="m-auto flex h-full max-w-[600px] flex-col gap-6 py-6">
       <section>
         <Skeleton className="mb-3 h-[24px] w-full rounded-lg" />
         <Skeleton className="mb-3 h-[60px] w-full rounded-lg" />
