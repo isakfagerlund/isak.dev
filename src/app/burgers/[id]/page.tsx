456d855fb1fe314002e975ad0c2bd27f7aab614a
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-03-24 20:42:31 +0100

    fix scrollbar

--- src/app/burgers/[id]/page.tsx
+++ src/app/burgers/[id]/page.tsx
@@ -13,11 +13,11 @@ export default async function Burger({ params }: { params: { id: string } }) {
   return (
     <div className="m-auto flex h-[calc(100vh-72px)] max-w-[600px] flex-col gap-6 pb-3">
       <section>
-        <p className="text-slate-500 lowercase">
+        <p className="lowercase text-slate-500">
           {burger.country?.split(" ")[0]}, {burger.city}{" "}
           {burger.country?.split(" ")[1]}
         </p>
-        <h1 className="scroll-m-20 pb-3 text-4xl font-extrabold tracking-tight lowercase lg:text-5xl">
+        <h1 className="scroll-m-20 pb-3 text-4xl font-extrabold lowercase tracking-tight lg:text-5xl">
           {burger.resturantName}
         </h1>
         <Rating rating={burger.rating ?? 0} />
