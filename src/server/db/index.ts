4478d5089d8414ba1e7791bcf76264dd47c56e4e
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-03-25 13:59:16 +0100

    fix

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,11 +2,11 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
 import * as schema from "./schema";
+import { env } from "~/env";
 
 const client = createClient({
-  url: "libsql://isak-dev-isakfagerlund.turso.io",
-  authToken:
-    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTA2MDk1NjgsImlkIjoiMzJhMTgyNjMtYTMxNy00NzI3LWE1YjAtNTVkN2FlYzg2ZjY1In0.P0SJtIRGUdARNwuGKU4tmsqPKOvlhxE2s5A3gE2RSZHuFYbuARHBANaISbEFVwRVddZi6StvR92R_bYC9HloBA",
+  url: env.TURSO_URL,
+  authToken: env.TURSO_TOKEN,
 });
 
 export const db = drizzle(client, { schema });
