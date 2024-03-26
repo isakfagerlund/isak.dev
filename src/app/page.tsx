6c92dd12dacbb4b082495a630827adacb3ad2ea2
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-03-26 22:03:58 +0100

    Add cafe link from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { BurgerIcon } from "./_components/BurgerIcon";
+import { Coffee } from "lucide-react";
 
 export default async function Home() {
   return (
@@ -8,12 +9,19 @@ export default async function Home() {
         <h1 className="text-2xl font-extrabold tracking-tight">
           Isak Fagerlund
         </h1>
-        <p className="tracking-tight text-slate-500">Software engineer</p>
+        <p className="tracking-tight text-slate-500">Developer</p>
       </div>
       <div className="absolute bottom-3 right-3 ">
         <Link href="/burgers" className="flex items-center gap-2">
           <p className="text-xs text-slate-500">Burgers</p>
           <BurgerIcon color />
+        </Link>
+      </div>
+
+      <div className="absolute bottom-3 left-3 ">
+        <Link href="/cafes" className="flex items-center gap-2">
+          <Coffee strokeWidth={1} color="#64748b" size={18} />
+          <p className="text-xs text-slate-500">Cafes</p>
         </Link>
       </div>
     </main>
