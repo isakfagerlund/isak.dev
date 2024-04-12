66345d8900ba969c7c1f9ee7e05729d9e445c317
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-04-12 12:41:42 +0200

    remove clerk

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,22 +1,12 @@
 "use client";
-import { ClerkProvider, UserButton } from "@clerk/nextjs";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col">
-      <ClerkProvider
-        appearance={{
-          elements: {
-            footer: "hidden",
-          },
-        }}
-      >
-        <header className="flex justify-between py-2">
-          <h2>Admin page</h2>
-          <UserButton />
-        </header>
-        {children}
-      </ClerkProvider>
+      <header className="flex justify-between py-2">
+        <h2>Admin page</h2>
+      </header>
+      {children}
     </div>
   );
 }
--- src/app/admin/sign-in/[[...sign-in]]/page.tsx
+++ src/app/admin/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,7 @@
-import { SignIn } from "@clerk/nextjs";
-
 export default function Page() {
   return (
     <div className="flex h-dvh w-full items-center justify-center">
-      <SignIn />
+      Sign in button here
     </div>
   );
 }
