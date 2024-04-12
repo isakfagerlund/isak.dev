e64e2b748a9f2572b9a7f268cdc365c08ca1caec
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-04-12 13:57:33 +0200

    fix

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,36 +1,18 @@
-"use client";
-import { DataTable } from "./data-table";
-import { AddBurger } from "./AddBurger";
-import { AddCafe } from "./AddCafe";
-import { burgerColumns } from "./burgerColumns";
-import { cafeColumns } from "./cafeColumns";
-import { api } from "~/trpc/react";
-import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import ClientPage from "./ClientPage";
+import { redirect } from "next/navigation";
 
-export default function BurgersAdmin() {
-  const { data: allBurgers, refetch: refetchBurgers } =
-    api.burger.getAll.useQuery();
-  const { data: allCafes, refetch: refetchCafes } = api.cafes.getAll.useQuery();
-  const { permissions } = useKindeBrowserClient();
+export default async function Admin() {
+  const { getPermissions } = getKindeServerSession();
+  const permissions = await getPermissions();
 
   if (!permissions?.permissions?.includes("edit")) {
-    return (
-      <div className="flex flex-col gap-3 pb-2">
-        <p>You are note allowed to access this page</p>
-      </div>
-    );
-  }
-
-  if (!allBurgers || !allCafes) {
-    return null;
+    redirect("/");
   }
 
   return (
     <div className="flex flex-col gap-3 pb-2">
-      <AddBurger refetchBurgers={refetchBurgers} />
-      <DataTable columns={burgerColumns} data={allBurgers} />
-      <AddCafe refetchCafes={refetchCafes} />
-      <DataTable columns={cafeColumns} data={allCafes} />
+      <ClientPage />
     </div>
   );
 }
