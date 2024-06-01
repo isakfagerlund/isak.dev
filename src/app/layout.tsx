236adf7d603a315c0a7e566cf29f402b6f3c9e27
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-06-01 18:10:26 +0200

    dvh to svh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
-        <main className="m-auto h-dvh ">
+        <main className="m-auto h-svh">
           <TRPCReactProvider>{children}</TRPCReactProvider>
         </main>
         <Toaster
